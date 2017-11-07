683141ef992635014a67cbc3881a6b22c2a9455c
Martens, Daniel (335604) <daniel.martens@t-systems.com>
2017-11-07 15:37:59 +0100

    Cleanup and change api-version.

--- util/version.go
+++ util/version.go
@@ -8,4 +8,4 @@ const Version string = "0.1.0"
 
 //APIVersion is the current api version.
 //This is used if the API might change in the future.
-const APIVersion string = "v1"
+const APIVersion string = "capoeira-v1"
