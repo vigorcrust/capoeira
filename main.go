f40f6c997c7f076776cb688bc46abf74a5cc1b54
Martens, Daniel (335604) <daniel.martens@t-systems.com>
2017-11-07 12:51:05 +0100

    Sorted flags and commands in help

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"os"
+	"sort"
 
 	"github.com/vigorcrust/capoeira/util"
 
@@ -15,11 +16,15 @@ func main() {
 	app.Version = util.Version
 	app.Author = "vigorcrust"
 	app.Email = "ddmartens@gmail.com"
-	app.Usage = ""
+	app.Usage = "is an application which lets you tunnel through restrictive proxies."
 
 	app.Flags = GlobalFlags
 	app.Commands = Commands
 	app.CommandNotFound = CommandNotFound
+
+	//Sort the flags and commands so that they are sorted when you print the help
+	sort.Sort(cli.FlagsByName(app.Flags))
+	sort.Sort(cli.CommandsByName(app.Commands))
 
 	app.Run(os.Args)
 }
